413dd9b750106df8da07c89d6e6ae46650ccb09b
Reda Arraji <rarraji@c1r8p4.1337.ma>
2024-06-30 10:52:00 +0100

    push6

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# installing flask version 2.1.0 from pip3
+# Installs flask v2.1.0 with pip3
+
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
